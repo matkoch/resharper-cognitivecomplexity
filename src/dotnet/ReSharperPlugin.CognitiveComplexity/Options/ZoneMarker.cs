233b6c3d09c8b3166590779037011144090147a3
Matthias Koch <ithrowexceptions@gmail.com>
2019-08-27 13:28:37 +0200

    Add zone for options page

---
+++ src/dotnet/ReSharperPlugin.CognitiveComplexity/Options/ZoneMarker.cs
@@ -0,0 +1,10 @@
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.Application.UI.Options.OptionPages;
+
+namespace ReSharperPlugin.CognitiveComplexity.Options
+{
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<IToolsOptionsPageImplZone>
+    {
+    }
+}
