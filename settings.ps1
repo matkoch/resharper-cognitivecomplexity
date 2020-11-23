48ba50aa1eb018b07f7ff2da973f8a926ea4b6b9
Matthias Koch <ithrowexceptions@gmail.com>
2020-11-24 00:50:02 +0100

    Update build

--- settings.ps1
+++ settings.ps1
@@ -1,4 +1,3 @@
-$PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 $PluginId = "ReSharperPlugin.CognitiveComplexity"
 $SolutionPath = "$PSScriptRoot\CognitiveComplexity.sln"
 $SourceBasePath = "$PSScriptRoot\src\dotnet"
