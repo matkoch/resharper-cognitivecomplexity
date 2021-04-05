8794ce578228b7e3801c6f071ddf07b98f0ed1ef
Matthias Koch <ithrowexceptions@gmail.com>
2021-04-05 18:52:36 +0200

    Update build

--- settings.ps1
+++ settings.ps1
@@ -2,7 +2,7 @@ $PluginId = "ReSharperPlugin.CognitiveComplexity"
 $SolutionPath = "$PSScriptRoot\CognitiveComplexity.sln"
 $SourceBasePath = "$PSScriptRoot\src\dotnet"
 
-$VsWhereOutput = [xml] (& "$PSScriptRoot\tools\vswhere.exe" -format xml)
+$VsWhereOutput = [xml] (& "$PSScriptRoot\tools\vswhere.exe" -format xml -latest -products *)
 $VisualStudio = $VsWhereOutput.instances.instance |
     Where-Object { $_.channelId -match "Release" } |
     Sort-Object -Property installationVersion |
@@ -11,7 +11,7 @@ $VisualStudio = $VsWhereOutput.instances.instance |
 $VisualStudioBaseDirectory = $VisualStudio.installationPath
 $VisualStudioMajorVersion = ($VisualStudio.installationVersion -split '\.')[0]
 $VisualStudioInstanceId = $VisualStudio.instanceId
-$DevEnvPath = Get-ChildItem "$VisualStudioBaseDirectory\Common7\IDE\devenv.exe"
+$DevEnvPath = Get-ChildItem "$VisualStudioBaseDirectory\*\IDE\devenv.exe"
 $MSBuildPath = Get-ChildItem "$VisualStudioBaseDirectory\MSBuild\*\Bin\MSBuild.exe"
 
 $OutputDirectory = "$PSScriptRoot\output"
