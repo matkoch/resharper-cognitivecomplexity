afc73fe3586f66b51019c292965399911e9a8424
Matthias Koch <ithrowexceptions@gmail.com>
2021-05-10 14:07:26 +0200

    Remove configurability

--- src/dotnet/ReSharperPlugin.CognitiveComplexity/ICognitiveComplexityZone.cs
+++ src/dotnet/ReSharperPlugin.CognitiveComplexity/ICognitiveComplexityZone.cs
@@ -6,7 +6,6 @@ using JetBrains.ReSharper.Psi.CSharp;
 namespace ReSharperPlugin.CognitiveComplexity
 {
     [ZoneDefinition]
-    [ZoneDefinitionConfigurableFeature("Title", "Description", IsInProductSection: false)]
     public interface ICognitiveComplexityZone : IPsiLanguageZone,
         IRequire<ILanguageCSharpZone>,
         IRequire<DaemonZone>
