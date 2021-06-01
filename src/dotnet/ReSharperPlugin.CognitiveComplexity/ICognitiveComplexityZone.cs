a97bb13648dd73ffac70886fcc5ebf87efce450f
Matthias Koch <ithrowexceptions@gmail.com>
2021-06-01 23:31:08 +0200

    update dotnet

--- src/dotnet/ReSharperPlugin.CognitiveComplexity/ICognitiveComplexityZone.cs
+++ src/dotnet/ReSharperPlugin.CognitiveComplexity/ICognitiveComplexityZone.cs
@@ -2,11 +2,17 @@ using JetBrains.Application.BuildScript.Application.Zones;
 using JetBrains.ReSharper.Feature.Services.Daemon;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CSharp;
+#if RIDER
+using JetBrains.Rider.Backend.Env;
+#endif
 
 namespace ReSharperPlugin.CognitiveComplexity
 {
     [ZoneDefinition]
     public interface ICognitiveComplexityZone : IPsiLanguageZone,
+#if RIDER
+        IRequire<IRiderPlatformZone>,
+#endif
         IRequire<ILanguageCSharpZone>,
         IRequire<DaemonZone>
     {
